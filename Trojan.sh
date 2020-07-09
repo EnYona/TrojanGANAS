a53d2b777d7f30d3a7289d42960e5d2b1e4905a3
EnYona <67996044+EnYona@users.noreply.github.com>
2020-07-09 16:35:56 +0700

    Add files via upload

---
+++ Trojan.sh
@@ -0,0 +1,236 @@
+#!/bin/bash
+
+toilet -f big -F gay Trojan
+
+echo "JANGAN SALAH GUNAKAN SCRIPT INI"
+read -p "MASUKAN NO TARGET : " pilih
+sleep 2
+	echo "\033[1;35m"
+	echo "( 1 ) SEND TROJAN GANAS"
+	echo "( 2 ) EXIT"
+	echo "[][][][][]"
+read -p "Send Trojan NO 1 : " pilih
+if [ $pilih = "1" ];
+then
+	echo "\033[1;31m"
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+	echo "Send Trojan  Berhasil"
+	sleep 1
+fi
+echo "\033[1;37m""
+elif [ $pilih = "2" ];
+then
+	echo "EXIT"
